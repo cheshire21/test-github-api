4f00450549bb5596aab0c51cb76ac68cec3ee0dc
cheshire21 <coren114@gmail.com>
2022-06-21 18:03:31 -0500

    feat: added controller and added githubmodule to app module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Req, Post, UseGuards } from '@nestjs/common';
+import { GithubWebhookGuard } from './github/github-webhook.guard';
+import { GithubService } from './github/github.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly githubService: GithubService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/webhooks/github')
+  @UseGuards(GithubWebhookGuard)
+  async(@Req() req: any) {
+    const headers = req.headers;
+    const body = req.body;
+    console.log('HEADERS: ', headers);
+    console.log('BODY: ', body);
+    if (headers['x-github-event'] === 'push') {
+      console.log(body.head_commit.message);
+
+      if (body.head_commit.message.toString().includes('challenge finished')) {
+        this.githubService.removeUserFromRepository(
+          body.repository.name,
+          body.pusher.name,
+        );
+      }
+    }
+    return body;
+  }
+
+  @Post('/create-repository')
+  async createRepository(
+    @Body() { repo, username }: any,
+  ): Promise<{ status: number; message: string }> {
+    await this.githubService.createRepository(repo);
+    await this.githubService.createReadme(repo);
+
+    await this.githubService.inviteUSerToSpecificRepository(repo, username);
+    await this.githubService.createWebhook(repo, ['push', 'pull_request']);
+
+    return { status: 200, message: 'Repository & readme created' };
   }
 }
