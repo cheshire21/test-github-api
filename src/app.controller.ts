721e7dcefa1e49886f535bb8340fd8f26a994d5f
cheshire21 <coren114@gmail.com>
2022-07-01 19:32:23 -0500

    feat: add endpoint to search a user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,34 @@
 import { Body, Controller, Req, Post, UseGuards } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { GithubWebhookGuard } from './github/github-webhook.guard';
 import { GithubService } from './github/github.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly githubService: GithubService) {}
+  constructor(
+    private readonly githubService: GithubService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('/webhooks/github')
   @UseGuards(GithubWebhookGuard)
   async(@Req() req: any) {
     const headers = req.headers;
     const body = req.body;
+    const message: string = body.head_commit.message;
     console.log('HEADERS: ', headers);
     console.log('BODY: ', body);
     if (headers['x-github-event'] === 'push') {
       console.log(body.head_commit.message);
 
-      if (body.head_commit.message.toString().includes('challenge finished')) {
+      if (
+        message.toLowerCase() ===
+        this.configService
+          .get('COMMIT_FINISH_CHALLENGE')
+          .toString()
+          .toLowerCase()
+          .trim()
+      ) {
         this.githubService.removeUserFromRepository(
           body.repository.name,
           body.pusher.name,
@@ -31,11 +43,19 @@ export class AppController {
     @Body() { repo, username }: any,
   ): Promise<{ status: number; message: string }> {
     await this.githubService.createRepository(repo);
-    await this.githubService.createReadme(repo);
+    await this.githubService.createReadme(
+      repo,
+      'https://ravndevelopment.notion.site/Ravn-iOS-Code-Challenge-d5302aa3791140dbae96cad236d8e5f5',
+    );
 
     await this.githubService.inviteUSerToSpecificRepository(repo, username);
     await this.githubService.createWebhook(repo, ['push', 'pull_request']);
 
     return { status: 200, message: 'Repository & readme created' };
+  }
+
+  @Post('/search')
+  async search(@Body('email') email: string) {
+    return await this.githubService.searchUser(email);
   }
 }
