c337bb151d1241e3bf5a90d0944d4a60ddcf6fc0
cheshire21 <coren114@gmail.com>
2022-07-01 19:31:46 -0500

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GithubModule } from './github/github.module';
 
 @Module({
-  imports: [GithubModule],
+  imports: [
+    GithubModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
